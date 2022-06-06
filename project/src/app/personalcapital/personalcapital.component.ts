eebf6d971f1466bf3fc63f7c5aace8531db2c909
Janisa Sri S <jani2995@global.chainsys.com>
2022-06-06 10:01:06 +0530

    first commit

--- project/src/app/personalcapital/personalcapital.component.ts
+++ project/src/app/personalcapital/personalcapital.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personalcapital',
   templateUrl: './personalcapital.component.html',
   styleUrls: ['./personalcapital.component.css']
 })
-export class PersonalcapitalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PersonalcapitalComponent  {}
--- project/src/app/savingplanner/savingplanner.component.ts
+++ project/src/app/savingplanner/savingplanner.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-savingplanner',
   templateUrl: './savingplanner.component.html',
   styleUrls: ['./savingplanner.component.css']
 })
-export class SavingplannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SavingplannerComponent  {}
